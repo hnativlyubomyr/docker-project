f98a5207f72c445c552a7a3c246f8c5ba0f4213e
hnativ <lhnat@softserveinc.com>
2022-11-30 09:42:34 +0200

    added posts module

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,8 @@
 import { createStore } from 'vuex'
+import { postsModule } from "@/store/postsModule";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    posts: postsModule,
   }
 })
