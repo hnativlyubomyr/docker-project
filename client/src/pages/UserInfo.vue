cc0d4b527d0bf5588d6fef29bde26e23c0a3148a
hnativ <lhnat@softserveinc.com>
2022-12-05 23:22:43 +0200

    updated UserInfo component

--- client/src/pages/UserInfo.vue
+++ client/src/pages/UserInfo.vue
@@ -1,15 +1,64 @@
 <template>
-  <div>
-      UserInfo component
+  <div class="user-info-container">
+    <div class="user-info">
+      <div class="user-title">User information:</div>
+
+      <div class="user-text">Name: {{ name }}</div>
+      <div class="user-text">Username: {{ username }}</div>
+      <div class="user-text">Password: {{ password }}</div>
+
+      <my-button
+          color-button="red"
+          class="logout-btn"
+          data-delete
+      >
+        Logout
+      </my-button>
+    </div>
   </div>
+
 </template>
 
 <script>
-export default {
+import { mapState } from "vuex";
 
+export default {
+  computed: {
+    ...mapState({
+      name: state => state.auth.name,
+      username: state => state.auth.username,
+      password: state => state.auth.password,
+    }),
+  }
 }
 </script>
 
 <style scoped>
+.user-info-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+}
+
+.user-info {
+  background-color: lightgray;
+  width: 300px;
+  padding: 20px 5px 10px 20px;
+}
+
+.user-title {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.user-text {
+  font-size: 20px;
+  margin-bottom: 5px;
+}
+
+.logout-btn {
+  margin-top: 10px;
+}
 
 </style>
